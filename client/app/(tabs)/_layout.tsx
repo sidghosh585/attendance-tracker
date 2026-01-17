0533c599fc9d2cf5a149b79b0bfad3d615f1deae
Sp-Sayan <sayanpaul09@gmail.com>
2026-01-17 13:35:54 +0530

    initial commit

---
+++ client/app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+export default function _layout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+}
