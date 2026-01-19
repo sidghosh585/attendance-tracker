501762d51d847ecaa341e3988602a190c0ce6b0e
Soumadeep19 <soumadeepputatunda@gmail.com>
2026-01-19 21:50:48 +0530

    updated index.tsx

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import "../global.css";
 export default function Index() {
   return (
     <View  className="flex-1 justify-center items-center">
-      <Text className="text-3xl font-bold text-cardForeground">Welcome</Text>
+      <Text className="text-3xl font-bold text-cardForeground">Welcome 2</Text>
       
   
     </View>
